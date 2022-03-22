a661851de0869f2d651a8602f7656b701420b173
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 15:01:51 -0300

    should have links navigation

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../helps/renderWithRouter';
+
+describe('App component test', () => {
+  it('should have links navigation', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+});
+
