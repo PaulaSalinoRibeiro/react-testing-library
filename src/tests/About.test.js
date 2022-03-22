535a3df125df99abd3ebdb70005c1792a15259e0
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 15:57:32 -0300

    verify image

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -17,4 +17,10 @@ describe('about component test', () => {
     const paragraph = screen.getAllByText(/pokémons/i);
     expect(paragraph).toHaveLength(2);
   });
+  it('verify image exist', () => {
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    expect(img).toBeDefined();
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(src);
+  });
 });
