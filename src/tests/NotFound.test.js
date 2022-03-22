4df3fa3aa516565445626abbdeb505f7de6d419f
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 16:49:25 -0300

    verify test NotFound component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helps/renderWithRouter';
+
+describe('NotFound component test', () => {
+  it('verify have text', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    history.push('/notfound');
+    const text = screen.getByText(/page requested not found/i);
+    expect(text).toBeDefined();
+
+    screen.logTestingPlaygroundURL();
+  });
+});
