6266aa14023eae4b57f044192e97ef64be6e1e0e
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 17:32:24 -0300

    verify next pokemon button

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helps/renderWithRouter';
+import App from '../App';
+
+describe('Pokedex component test', () => {
+  it('verify title', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByText(/encountered pokémons/i);
+    expect(title).toBeInTheDocument();
+
+    //screen.logTestingPlaygroundURL();
+  });
+
+  it.only('verify next pokemon', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(nextButton);
+    const nextPokemon = screen.getByText(/charmander/i);
+    expect(nextPokemon).toBeInTheDocument();
+
+    const clicks = 8;
+    for (let i = 0; i < clicks; i += 1) {
+      userEvent.click(nextButton);
+    }
+    const firstPokemon = screen.getByText(/pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
+  });
+});
