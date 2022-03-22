9a5e6b33c15a6d4addbb9edc4022a1e2cf2e56ab
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 16:58:21 -0300

    not found component test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,11 +4,22 @@ import NotFound from '../components/NotFound';
 import renderWithRouter from '../helps/renderWithRouter';
 
 describe('NotFound component test', () => {
-  it('verify have text', () => {
+  beforeEach(() => {
     const { history } = renderWithRouter(<NotFound />);
     history.push('/notfound');
+  });
+
+  it('verify have text', () => {
     const text = screen.getByText(/page requested not found/i);
     expect(text).toBeDefined();
+
+    screen.logTestingPlaygroundURL();
+  });
+  it('verify have image in the page', () => {
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    expect(image.src).toBe(src);
 
     screen.logTestingPlaygroundURL();
   });
