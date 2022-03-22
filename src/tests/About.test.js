f1e216f9c2e1e9325676eb8d56a034f8292b2125
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 15:41:33 -0300

    verify title about component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from '../helps/renderWithRouter';
+
+describe('about component test', () => {
+  it('verify title exist', () => {
+    const { history } = renderWithRouter(<About />);
+    history.push('/about');
+    const title = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(title).toBeDefined();
+  });
+});
