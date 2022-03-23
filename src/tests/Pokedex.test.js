8d828744d90598b099f842c3d269a67f26b29ee5
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-22 21:20:15 -0300

    verify pokemon card

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -15,17 +15,12 @@ describe('Pokedex component test', () => {
     renderWithRouter(<App />);
     const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
     expect(nextButton).toHaveTextContent(/próximo pokémon/i);
-    userEvent.click(nextButton);
-    const nextPokemon = screen.getByText(/charmander/i);
-    expect(nextPokemon).toBeInTheDocument();
 
-    const clicks = 8;
-    for (let i = 0; i < clicks; i += 1) {
-      userEvent.click(nextButton);
-    }
 
-    const firstPokemon = screen.getByText(/pikachu/i);
-    expect(firstPokemon).toBeInTheDocument();
+
+
+
+
   });
 
   it('verify if just one pokemom is render', () => {
@@ -36,30 +31,6 @@ describe('Pokedex component test', () => {
 
   it('should have filter buttons', () => {
     renderWithRouter(<App />);
-    const allBtn = screen.getByRole('button', { name: /all/i });
-    const electricBtn = screen.getByRole('button', { name: /electric/i });
-    const fireBtn = screen.getByRole('button', { name: /fire/i });
-    const bugBtn = screen.getByRole('button', { name: /bug/i });
-    const poisonBtn = screen.getByRole('button', { name: /poison/i });
-    const psychicBtn = screen.getByRole('button', { name: /psychic/i });
-    const normalBtn = screen.getByRole('button', { name: /normal/i });
-    const dragonBtn = screen.getByRole('button', { name: /dragon/i });
-    expect(allBtn).toBeInTheDocument();
-    expect(electricBtn).toBeInTheDocument();
-    expect(fireBtn).toBeInTheDocument();
-    expect(bugBtn).toBeInTheDocument();
-    expect(poisonBtn).toBeInTheDocument();
-    expect(psychicBtn).toBeInTheDocument();
-    expect(normalBtn).toBeInTheDocument();
-    expect(dragonBtn).toBeInTheDocument();
-
-    userEvent.click(psychicBtn);
-    const pokemonPsychic1 = screen.getByText(/alakazam/i);
-    expect(pokemonPsychic1).toBeInTheDocument();
-    const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
-    userEvent.click(nextButton);
-    const pokemonPsychic2 = screen.getByText(/mew/i);
-    expect(pokemonPsychic2).toBeInTheDocument();
     expect(allBtn).not.toBeDisabled();
   });
 
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helps/renderWithRouter';
+import App from '../App';
+
+describe('Pokemon component test', () => {
+  it('verify if show pokemon card', () => {
+    renderWithRouter(<App />);
+    const name = screen.getByTestId('pokemon-name');
+    const type = screen.getByTestId('pokemon-type');
+    const weight = screen.getByTestId('pokemon-weight');
+    const img = screen.getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+
+    expect(name).toHaveTextContent('Pikachu');
+    expect(type).toHaveTextContent('Electric');
+    expect(weight).toHaveTextContent('Average weight: 6.0 kg');
+    expect(img.src).toEqual(src);
+  });
+});
